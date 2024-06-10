4bae99dca411fe9e4e26a116409b1fb8158c0ce9
표승현 <jeongmok99@koreatech.ac.kr>
2024-06-10 23:13:08 +0900

    openCV LIB linking needed! :(

--- src/test1.cpp
+++ src/test1.cpp
@@ -3,7 +3,7 @@
 
 #include <eigen3/Eigen/Dense>
 #include <opencv2/core/core.hpp>
-#include <opencv2/core/eigen.hpp>
+//#include <opencv2/core/eigen.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/opencv.hpp>
 
